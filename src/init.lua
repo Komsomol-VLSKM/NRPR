0fff49695d8edfdbeab27812e078c65cc093ee26
A. Poporez <lithuanianball1@gmail.com>
2026-06-04 02:58:04 +0300

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -1,14 +1,22 @@
 local Players = game:GetService("Players")
-local ProvinceBorders = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Komsomol-VLSKM/NRPR/refs/heads/main/src/ProvinceBorders.lua"))--require(game.ReplicatedStorage.ProvinceBorders)
-local ProvinceBorderPoints = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Komsomol-VLSKM/NRPR/refs/heads/main/src/ProvinceBorderPoints.lua"))--require(game.ReplicatedStorage.ProvinceBorderPoints)
+print("running")
+local ProvinceBorders = loadstring(game:HttpGet("https://raw.githubusercontent.com/Komsomol-VLSKM/NRPR/refs/heads/main/src/ProvinceBorders.lua"))()
+print("1")
+local ProvinceBorderPoints = loadstring(game:HttpGet("https://raw.githubusercontent.com/Komsomol-VLSKM/NRPR/refs/heads/main/src/ProvinceBorderPoints.lua"))()
+print("2")
+local RemoteCalls = loadstring(game:HttpGet("https://raw.githubusercontent.com/Komsomol-VLSKM/NRPR/refs/heads/main/src/RemoteCalls.lua"))()
+print("3")
+local RunService = game:GetService("RunService")
 
-local provincesFolder = workspace.Germany
+local Unit = loadstring(game:HttpGet("https://raw.githubusercontent.com/Komsomol-VLSKM/NRPR/refs/heads/main/src/class/Unit.lua"))()
+
+local provincesFolder = workspace.Map
 
 local player = Players.LocalPlayer
 
-player.CharacterAdded:Wait()
+--player.CharacterAdded:Wait()
 
-wait(3)
+--wait(3)
 print("loading map")
 
 local index = ProvinceBorders.BuildIndex(provincesFolder)
@@ -17,26 +25,41 @@ print("map loaded")
 local player = game.Players.LocalPlayer
 local mouse = player:GetMouse()
 
+local function spawnTroop()
+	local args = {
+		workspace:WaitForChild("Map"):WaitForChild("Province"),
+		vector.create(-21.56826400756836, -58.49993896484375, 86.84256744384766),
+		"Troop"
+	}
+	game:GetService("ReplicatedStorage"):WaitForChild("SpawnTroop"):FireServer(unpack(args))
+	
+end
+
 mouse.Button1Down:Connect(function()
 	local target = mouse.Target
 	if not target then return end
 	if target.Parent ~= provincesFolder then return end
 	
-	target.Color = Color3.new(1, 0, 0)
 	local bordering = ProvinceBorders.GetBorderingProvinces(target, index)
 	
 	local borderPoints = {}
 	
 	for _, neighbor in ipairs(bordering) do
-		table.insert(borderPoints, ProvinceBorderPoints.GetBorderPoint(target, neighbor, index))
+		table.insert(borderPoints, {ProvinceBorderPoints.GetBorderPoint(target, neighbor, index, nil, 1)})
 	end
 	
-	for _, point in ipairs(borderPoints) do
-		local part = Instance.new("Part")
-		part.Size = Vector3.new(1, 1, 1)
-		part.Position = Vector3.new(point.x, 1, point.z)
-		part.Anchored = true
-		part.Parent = workspace
-		part.BrickColor = BrickColor.new("Bright red")
+	for _, borderPoint in ipairs(borderPoints) do
+		local pointY = -58.5
+		if RunService:IsStudio() then
+			pointY = 1
+		end
+		
+		local point = borderPoint[1]
+		
+		local unit = Unit.new("Troop", Vector3.new(point.x, pointY, point.z), target)
+		
+		local point = borderPoint[2]
+		
+		unit:move(Vector3.new(point.x, pointY, point.z))
 	end
 end)
